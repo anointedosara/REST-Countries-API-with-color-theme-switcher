9d83b1a13ab98889ccc5838734c218b5b74ba184
Anointed Osara <anointedosara@gmail.com>
2025-12-12 22:52:26 +0100

    first commit

--- src/app/[details]/loading.js
+++ src/app/[details]/loading.js
@@ -2,7 +2,7 @@
 export default function Loading() {
     return (
         <div className="flex items-center justify-center">
-            <h4 className="text-3xl font-bold">Loading Country Details...</h4>
+            <h4 className="text-3xl font-bold text-gray-950 dark:text-white">Loading Country Details...</h4>
         </div>
     )
 }
--- src/app/loading.js
+++ src/app/loading.js
@@ -8,7 +8,7 @@ export default function Loading() {
         <div className="grid w-full grid-cols-1 gap-10 m-auto max-w-75 md:max-w-325 md:grid-cols-3 lg:grid-cols-4 md:gap-15 lg:gap-17">
             {
                 data.map((item, i) => 
-                <div className="w-full bg-white shadow-md h-85" key={i}>
+                <div className="w-full bg-white rounded-md shadow-md dark:bg-blue-900 h-85" key={i}>
                 </div>)
             }
         </div>
